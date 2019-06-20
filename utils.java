8ed3059fde260630d3f62dc9cb4d3fc28595fba2
nareshdara <nareshdara200@gmail.com>
2019-06-20 16:43:12 +0530

    updated file

--- utils.java
+++ utils.java
@@ -1,3 +1,5 @@
 this is utils file it contains some java information
 this is very easy to learn
 it is very simple.
+it is more useful while preparing,you will get very good opturnity from this side
+so every one can teach and learn quickly.
