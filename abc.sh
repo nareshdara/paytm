c02166a83f95bd69b0b9bac6027e2c51dcbcd1c5
nareshdara <nareshdara200@gmail.com>
2019-06-21 15:28:45 +0530

    Revert "this is abc test message"
    This reverts commit aaa7ab18f08e228db0eb948f56b25afc20a88ca8.

--- abc.sh
+++
@@ -1,5 +0,0 @@
-this file contains some information
-the information which allocate in every accept of my life 
-so the infomation is very importent.
-you should give corect permission for every one.
-for that i am giving a root permission to every one in this dorectory.
